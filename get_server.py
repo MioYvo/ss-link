996664b7763e5d4cda4e911f48cf4cf0acd38c77
rusi.liu <liurusi@123feng.com>
2016-06-06 15:30:14 +0800

    改用新的获取所有节点接口

--- get_server.py
+++ get_server.py
@@ -19,8 +19,17 @@ def login(user, password):
                  'password': md5(password),
                  'redirect': '/my'}
     login_uri = 'http://www.ss-link.com/login'
+    hdr = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, sdch, br",
+        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+        "Connection": "keep-alive",
+        "Host": "www.ss-link.com",
+        "Referer": "https://www.ss-link.com/login",
+        "Upgrade-Insecure-Requests": "1",
+    }
 
-    login_rst = requests.post(login_uri, files=user_info)
+    login_rst = requests.post(login_uri, files=user_info, headers=hdr)
     if login_rst.status_code != 200:
         raise Exception(login_rst.text)
     else:
@@ -43,61 +52,6 @@ def get_hosts(login_rst):
 def parse_html(html):
     selector = etree.HTML(html)
     a = selector.xpath('//*[@id="main"]/script[6]/text()')[0]
-
-    # a = """
-    # jQuery( document ).ready(function() {
-    #         jQuery('#main').data('myarray', []);
-    #         jQuery('#main').data('n', []);
-    #             jQuery('#main').data('myarray').push("45.35.71.119");
-    #             n="美国洛杉矶PS线路, 节点45.35.71.119 , 当前负载423";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("45.35.75.57");
-    #             n="美国洛杉矶PS线路, 节点45.35.75.57 , 当前负载423";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("45.35.45.35");
-    #             n="美国达拉斯PD线路, 节点45.35.45.35 , 当前负载429";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("204.74.217.107");
-    #             n="美国硅谷T2线路, 节点204.74.217.107 , 当前负载454";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("204.74.212.195");
-    #             n="美国硅谷T2线路, 节点204.74.212.195 , 当前负载455";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("204.74.215.39");
-    #             n="美国硅谷T2线路, 节点204.74.215.39 , 当前负载455";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("204.74.216.105");
-    #             n="美国硅谷T2线路, 节点204.74.216.105 , 当前负载455";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("72.46.135.119");
-    #             n="美国拉斯维加斯VW线路, 节点72.46.135.119 , 当前负载473";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("173.252.220.201");
-    #             n="美国硅谷T2线路, 节点173.252.220.201 , 当前负载553";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("104.250.144.10");
-    #             n="美国洛杉矶GR线路, 节点104.250.144.10 , 当前负载553";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("76.164.224.102");
-    #             n="美国拉斯维加斯VW线路, 节点76.164.224.102 , 当前负载363";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("104.250.143.243");
-    #             n="美国洛杉矶GR线路, 节点104.250.143.243 , 当前负载365";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("104.250.146.212");
-    #             n="美国洛杉矶GR线路, 节点104.250.146.212 , 当前负载365";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("104.250.146.37");
-    #             n="美国洛杉矶GR线路, 节点104.250.146.37 , 当前负载366";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("104.250.147.22");
-    #             n="美国洛杉矶GR线路, 节点104.250.147.22 , 当前负载367";
-    #             jQuery('#main').data('n').push(n);
-    #             jQuery('#main').data('myarray').push("46.249.56.39");
-    #             n="欧洲荷兰线路, 节点46.249.56.39 , 当前负载397";
-    #             jQuery('#main').data('n').push(n);
-    # });
-    # """
     b = a.split("push(")
 
     host_dict = {}
@@ -120,60 +74,96 @@ def parse_html(html):
     return host_dict
 
 
-def ping_one(ip, ping_count=None):
+def ping_one(ip, ping_count=None, su=False):
     if not ping_count:
         ping_count = 3
-    r = pyping.ping(ip, count=ping_count, udp=True)
-    return {"ip": ip, "avg_rtt": r.avg_rtt}
+    r = pyping.ping(ip, count=ping_count, udp=not su)
+    _, geo_ip, isp, server = loc_from_ip_cn(ip)
+    return {"ip": ip, "avg_rtt": float(r.avg_rtt) if r.avg_rtt else None, "geo_ip": geo_ip, "isp": isp,
+            "server": server}
+
+
+def get_all_hosts():
+    url = "https://www.ss-link.com/all"
+    res = requests.get(url)
+    assert res.ok
+    # for ip in res.content.splitlines():
+    #     ip = ip.strip()
+    #     loc = loc_from_ip_cn(ip)
+    return [ip.strip() for ip in res.content.splitlines()]
+
+
+def loc_from_ip_cn(ip):
+    """
+    通过ip.cn解析ip位置
+    :param ip:
+    :return:
+    """
+    url = "http://ip.cn/index.php"
+    loc_res = requests.get(url, params={"ip": ip})
+    tree = etree.HTML(loc_res.content)
+    isp = tree.xpath('//*[@id="result"]/div/p[1]/text()[2]')[0]
+    geo_ip = tree.xpath('//*[@id="result"]/div/p[2]')[0]
+    server = tree.xpath('//*[@id="result"]/div/p[3]')[0]
+    return ip, geo_ip.text, isp, server.text
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--user", help="account(email)", )
-    parser.add_argument("-p", "--password", help="password")
-    parser.add_argument("-c", "--ping count", help="ping count", type=int)
+    # parser.add_argument("-u", "--user", help="account(email)", )
+    # parser.add_argument("-p", "--password", help="password")
+    parser.add_argument("-c", "--ping_count", help="ping count", type=int, default=10)
+    parser.add_argument("-s", '--su', help='is su do ?', type=bool, default=False)
     args = parser.parse_args()
-    if not (args.user and args.password):
-        print("enter your information please")
-        user = raw_input("ss-link user: ")
-        password = raw_input("ss-link password: ")
-    else:
-        user, password = args.user, args.password
-
-    if not (user and password):
-        raise Exception("没有密码搞毛啊!")
-    _login_rst = login(user, password)
-    _html = get_hosts(_login_rst)
-    # _html = get_hosts(None)
-    _host = parse_html(_html)
+    # if not (args.user and args.password):
+    #     print("enter your information please")
+    #     user = raw_input("ss-link user: ")
+    #     password = raw_input("ss-link password: ")
+    # else:
+    #     user, password = args.user, args.password
+    #
+    # if not (user and password):
+    #     raise Exception("没有密码搞毛啊!")
+    # _login_rst = login(user, password)
+    # _html = get_hosts(_login_rst)
+    # _host = parse_html(_html)
 
     # pool = Pool(cpu_count())
-    pool = dummy.Pool(cpu_count())
-    print "--> start {} threads\n".format(cpu_count())
-    multiple_results = [pool.apply_async(ping_one, (_ip, args.ping_count)) for _ip in _host]
+    _host = get_all_hosts()
+    for i in _host:
+        print i
 
-    _host_rst = _host.copy()
+    print '\n--> got {} hosts to parse\n'.format(len(_host))
+
+    pool = dummy.Pool(cpu_count())
+    print "-->>> su: {}".format(args.su)
+    print "--> start {} threads\n".format(cpu_count())
+    multiple_results = [pool.apply_async(ping_one, (_ip, args.ping_count, args.su)) for _ip in _host]
+
+    _host_rst = {}
     with tqdm(total=len(_host), ncols=100, smoothing=0.1, dynamic_ncols=True) as pbar:
         for res in multiple_results:
             ping_rst = res.get()
-            _host_rst[ping_rst['ip']]['avg_rtt'] = float(ping_rst['avg_rtt']) if ping_rst['avg_rtt'] else "None"
+            _host_rst[ping_rst['ip']] = ping_rst
             pbar.update(1)
 
-    sorted_host = sorted(_host_rst.items(), key=lambda t: t[1]['avg_rtt'], reverse=True)
+    sorted_host = sorted(_host_rst.iteritems(), key=lambda t: t[1]['avg_rtt'], reverse=True)
     print "\n"
     for h in sorted_host:
-        print "ip: {:<17} | load: {:<20} | avg_rtt: {:<20} | name: {} ".format(h[0], h[1]['load'],
-                                                                               str(h[1]['avg_rtt']), h[1]['name'])
+        print "ip: {:<17} | avg_rtt: {:<17} | {:<50} | {:<25} | {}".format(h[0], str(h[1]['avg_rtt']), h[1]['geo_ip'],
+                                                                           h[1]['server'], h[1]['isp'])
 
-    h_fastest = "ip: {:<17} | load: {:<20} | avg_rtt: {:<20} | name: {} ".format(sorted_host[-1][0],
-                                                                                 sorted_host[-1][1]['load'],
-                                                                                 sorted_host[-1][1]['avg_rtt'],
-                                                                                 sorted_host[-1][1]['name'])
+    h_fastest = "ip: {:<17} | avg_rtt: {:<17} | {:<50} | {:<25} | {}".format(sorted_host[-1][0],
+                                                                             sorted_host[-1][1]['avg_rtt'],
+                                                                             sorted_host[-1][1]['geo_ip'],
+                                                                             sorted_host[-1][1]['server'],
+                                                                             sorted_host[-1][1]['isp'])
 
     print "\n"
     print "fastest host:\n"
     print h_fastest
-    print "\n bye!"
+    print "\nbye!"
+
 
 if __name__ == '__main__':
     main()
